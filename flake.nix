89c6a1c29b4f8d809440748c88433cadb226c31b
Chih Wang <ongchi@users.noreply.github.com>
2024-12-24 21:27:35 +0800

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = {
+    self,
+    fenix,
+    flake-utils,
+    nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      toolchain = with fenix.packages.${system};
+        combine [
+          stable.cargo
+          stable.clippy
+          stable.rust-src
+          stable.rustc
+          stable.rustfmt
+          rust-analyzer
+        ];
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          toolchain
+          pkgs.libiconv
+        ];
+        packages =
+          [
+            pkgs.nodejs_23
+          ]
+          ++ (with pkgs.nodePackages; [pnpm]);
+      };
+    });
+}
