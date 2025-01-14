110aec46f5512d7f3d56626815dc849869b244d4
Chih Wang <ongchi@users.noreply.github.com>
2025-01-14 21:07:30 +0800

    add clear button for plot

--- web/src/sensor-data/sensor-data.svelte
+++ web/src/sensor-data/sensor-data.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  import { selectedTaskInfo } from "$lib/shared-variables.svelte";
+  import { Button } from "$lib/components/ui/button";
+  import { pgClient, selectedTaskInfo } from "$lib/shared-variables.svelte";
+  import { toast } from "svelte-sonner";
 
   let baseUrl = "http://localhost:80/troll_sensor_data/";
 
@@ -20,7 +22,33 @@
 </script>
 
 {#if selectedTaskInfo.length > 0}
+  <div class="flex w-full">
+    <div class="flex px-2 ml-auto">
+      <Button
+        variant="destructive"
+        size="sm"
+        onclick={() => {
+          pgClient
+            .from("sensor_data")
+            .delete()
+            .eq("task_id", selectedTaskInfo[0]?.task_id)
+            .then(({ error }) => {
+              if (error) {
+                toast.error(error.message);
+              } else {
+                toast.success("Data cleared");
+                let iframe = document.getElementById(
+                  "plot",
+                ) as HTMLIFrameElement;
+                iframe.src = iframe.src;
+              }
+            });
+        }}>Clear</Button
+      >
+    </div>
+  </div>
   <div class="w-full px-2">
-    <iframe src={iframeSrc} title="Plotting" class="w-full h-[85em]"></iframe>
+    <iframe id="plot" src={iframeSrc} title="Plotting" class="w-full h-[85em]"
+    ></iframe>
   </div>
 {/if}
