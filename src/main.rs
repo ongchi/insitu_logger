c3deddd4cbe7f7c7bafd5528ec16b50c0276e838
Chih Wang <ongchi@users.noreply.github.com>
2025-02-24 14:49:44 +0800

    add api endpoint of insitu_log_handler

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 use axum::{
+    extract::{DefaultBodyLimit, Multipart},
     http::{header, HeaderValue, StatusCode, Uri},
-    response::{IntoResponse, Response},
-    routing::{get, Router},
+    response::{IntoResponse, Response, Result},
+    routing::{get, post, Router},
+    Json,
 };
+use insitu_log_reader::InSituLogReader;
 use rust_embed::RustEmbed;
-use std::net::SocketAddr;
+use std::{io::Cursor, net::SocketAddr};
 use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
     // Server routes
     let app = Router::new()
-        .route("/ping", get(|| async { "pong" }))
         .route("/", get(index_handler))
+        .route("/insitu_log", post(insitu_log_handler))
         .route("/{*path}", get(static_handler))
         .layer(CorsLayer::new().allow_origin([
             HeaderValue::from_static("http://localhost:4000"),
             HeaderValue::from_static("http://localhost:5173"),
-        ]));
+        ]))
+        .layer(DefaultBodyLimit::max(100 * 1000 * 1000));
 
     // Start server
     let addr = SocketAddr::from(([0, 0, 0, 0], 4000));
@@ -30,6 +34,30 @@ async fn main() {
 
 async fn index_handler() -> impl IntoResponse {
     static_handler(Uri::from_static("/index.html")).await
+}
+
+async fn insitu_log_handler(mut multipart: Multipart) -> Result<Json<InSituLogReader>> {
+    let log = if let Some(field) = multipart.next_field().await.unwrap() {
+        let file_name = field.file_name().unwrap_or("").to_string();
+        let data = field.bytes().await.unwrap();
+        let ext = file_name.split('.').last().unwrap_or("");
+
+        let mut reader = Cursor::new(data);
+
+        match ext {
+            "csv" => InSituLogReader::from_csv(&mut reader)
+                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
+            "txt" => InSituLogReader::from_txt(&mut reader)
+                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
+            "zip" => InSituLogReader::from_zipped_html(&mut reader)
+                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
+            _ => return Err(StatusCode::BAD_REQUEST)?,
+        }
+    } else {
+        return Err(StatusCode::BAD_REQUEST)?;
+    };
+
+    Ok(Json(log))
 }
 
 async fn static_handler(uri: Uri) -> impl IntoResponse {
