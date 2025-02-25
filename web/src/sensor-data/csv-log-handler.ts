a3edeec83a897573c1af3ac721ffde8d5508fbc8
Chih Wang <ongchi@users.noreply.github.com>
2025-02-25 14:20:02 +0800

    add support for InSitu txt dump format

--- web/src/sensor-data/csv-log-handler.ts
+++ web/src/sensor-data/csv-log-handler.ts
@@ -1,6 +1,7 @@
 import { type InSituLog } from "$lib/types.ts";
 
 let COLUMN_NAME_MAPPING: { [key: string]: string } = {
+  // csv log column names
   "Date/Time": "datetime",
   "Temp": "temp",
   "Pres": "pres",
@@ -17,6 +18,24 @@ let COLUMN_NAME_MAPPING: { [key: string]: string } = {
   "PPO2": "ppo2",
   "Batt Perc(%)": "batt",
   "R": "resis",
+  // txt dump column names
+  "Date and Time": "datetime",
+  "Temperature": "temp",
+  "External Voltage": "v",
+  "Battery Percentage (%)": "batt",
+  "Barometric Pressure": "pres_baro",
+  "Pressure": "pres",
+  "Dissolved Oxygen (concentration)": "do_con",
+  "Partial Pressure Oxygen": "ppo2",
+  "pH(mV)": "ph_mv",
+  "Dissolved Oxygen (%saturation)": "do_sat",
+  "Oxidation Reduction Potential (ORP)": "orp",
+  "Actual Conductivity": "cndct",
+  "Specific Conductivity": "spcndct",
+  "Salinity": "sa",
+  "Resistivity": "resis",
+  "Water Density": "wtr_d",
+  "Total Dissolved Solids": "tds",
 }
 
 let UNIT_MODIFIERS: { [key: string]: number } = {
@@ -30,12 +49,15 @@ let UNIT_MODIFIERS: { [key: string]: number } = {
   "(PSU)": 1.0,
   "(ppm)": 1.0,
   "(ppt)": 1000.0,
-  "(pH)": 1.0, "(mV)": 1.0,
+  "(pH)": 1.0,
+  "(mV)": 1.0,
   "(mg/L)": 1.0,
   "(%Sat)": 1.0,
   "(NTU)": 1.0,
   "(Torr)": 1.0,
   "(ohm-cm)": 1.0,
+  "(V)": 1.0,
+  "(g/cm3)": 1.0,
 }
 
 export function insitu_log_handler(log: InSituLog): { [key: string]: number[] } {
@@ -48,13 +70,12 @@ export function insitu_log_handler(log: InSituLog): { [key: string]: number[] } 
 
   columnNames.forEach((name, i) => {
     Object.keys(COLUMN_NAME_MAPPING).forEach(key => {
-      if (name.startsWith(key)) {
+      if (name === key || name.replace(/(\s?\([^\)]*\)$)/, '') === key) {
         sqlColNames.push(COLUMN_NAME_MAPPING[key]);
         let unit = name.replace(key, "").trim();
         if (unit === "") {
           unitModifiers.push(1.0)
-        }
-        else if (unit in UNIT_MODIFIERS) {
+        } else if (unit in UNIT_MODIFIERS) {
           unitModifiers.push(UNIT_MODIFIERS[unit]);
         } else {
           throw new Error(`Unknown unit: ${unit}`);
@@ -62,7 +83,6 @@ export function insitu_log_handler(log: InSituLog): { [key: string]: number[] } 
       }
     })
     if (sqlColNames.length !== i + 1) {
-      console.log(`Unknown column name: ${name}`);
       sqlColNames.push("");
       unitModifiers.push(0.0);
     }
